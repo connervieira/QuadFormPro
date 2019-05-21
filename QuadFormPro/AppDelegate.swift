0d9f8b09cfafe52a72950a5a1a4779eedb4344d1
Conner Vieira <vieirast@icloud.com>
2019-05-21 16:29:12 -0400

    Initial Commit

---
+++ QuadFormPro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuadFormPro
+//
+//  Created by Conner Vieira on 5/21/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
